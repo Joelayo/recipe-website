07255cea8a6d1f51fcf326f4fc1a13d3b4576351
Adedotun Aderibigbe <adedezzy@gmail.com>
2023-04-13 21:54:33 +0100

    Corrections made

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { RecipeComponent } from './pages/recipe/recipe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'contact', component: ContactComponent},
+{path: 'home', component: HomeComponent},
+{path: 'blog', component: BlogComponent},
+{path: 'recipe', component: RecipeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RecipeComponent } from './pages/recipe/recipe.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RecipeComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
